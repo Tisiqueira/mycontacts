2b91777c99be30f89666fc4daed2eeda5db919de
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2024-07-09 03:43:55 +0000

    change config style

--- fe/src/assets/styles/global.js
+++ fe/src/assets/styles/global.js
@@ -12,6 +12,7 @@ export default createGlobalStyle`
     body {
         background:${({ theme }) => theme.backGroundColor};
         font-size: 16px;
+
     }
 
     button {
--- fe/src/components/Header/style.js
+++ fe/src/components/Header/style.js
@@ -15,7 +15,7 @@ export const InputSearchContainer = styled.div`
         width: 100%;
         background: #fff;
         border: none;
-        border-radius: 25px;
+        border-radius: 50px;
         height: 50px;
         box-shadow: 0px 4px 10px rgba(0, 0, 0.04);
         outline: 0;

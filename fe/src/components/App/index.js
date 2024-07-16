4d1cc064a50bb6c5439fb9545474e619eb7e3f8d
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2024-07-16 02:02:34 +0000

    create a Routes

--- fe/src/components/App/index.js
+++ fe/src/components/App/index.js
@@ -1,23 +1,26 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
 import Header from '../Header';
-import ContactsList from '../ContactsList';
+import Routes from '../../Routes';
 
 import { Container } from './styles';
 
 function App() {
     return (
-        <ThemeProvider theme={defaultTheme}>
-            <GlobalStyles />
+        <BrowserRouter>
+            <ThemeProvider theme={defaultTheme}>
+                <GlobalStyles />
 
-            <Container>
-                <Header />
-                <ContactsList />
-            </Container>
-        </ThemeProvider>
+                <Container>
+                    <Header />
+                    <Routes />
+                </Container>
+            </ThemeProvider>
+        </BrowserRouter>
     );
 }
 

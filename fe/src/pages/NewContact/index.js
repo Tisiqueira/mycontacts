2452397ab727af045020022a2e1b7d916c9f2601
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2024-07-18 02:40:55 +0000

    create component button

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -2,6 +2,7 @@ import PageHeader from '../../components/PageHeader';
 
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export default function NewContact() {
     return (
@@ -10,7 +11,14 @@ export default function NewContact() {
             <Input placeholder="Nome" />
             <Select>
                 <option value="123">Instagram</option>
+                <option value="123">Instagram</option>
+                <option value="123">Instagram</option>
+                <option value="123">Instagram</option>
             </Select>
+            <Button type="button">Salvar Alterações</Button>
+            <Button type="button" disabled>
+                Salvar Alterações
+            </Button>
         </>
     );
 }

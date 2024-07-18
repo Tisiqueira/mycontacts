f18ea1331baad3a76ed18bb3b86d7239813e8039
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2024-07-18 02:27:35 +0000

    create component Input and Select

--- fe/src/pages/Home/styles.js
+++ fe/src/pages/Home/styles.js
@@ -17,7 +17,7 @@ export const InputSearchContainer = styled.div`
         border: none;
         border-radius: 25px;
         height: 50px;
-        box-shadow: 0px 4px 10px rgba(0, 0, 0.04);
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
         outline: 0;
         padding: 0 16px;
 
--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,5 +1,16 @@
 import PageHeader from '../../components/PageHeader';
 
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
 export default function NewContact() {
-    return <PageHeader title="Novo contato" />;
+    return (
+        <>
+            <PageHeader title="Novo contato" />
+            <Input placeholder="Nome" />
+            <Select>
+                <option value="123">Instagram</option>
+            </Select>
+        </>
+    );
 }

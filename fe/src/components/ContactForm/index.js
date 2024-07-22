034bbf96f66257c408bf59e8c3534f7fcd753d07
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2024-07-22 02:00:50 +0000

    Add new components

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,3 +1,10 @@
+import { Form } from './styles';
+import FormGroup from '../FormGroup';
+
 export default function ContactForm() {
-    return 'ContactForm';
+    return (
+        <Form>
+            <FormGroup />
+        </Form>
+    );
 }
---
+++ fe/src/components/FormGroup/index.js
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+import Input from '../Input';
+
+export default function FormGroup() {
+    return (
+        <Container>
+            <Input />
+        </Container>
+    );
+}
